783fca9604745309b3109bb8be760db311545755
Long Le <lplong31@gmail.com>
2022-06-14 00:56:44 +0700

    1604- implement meaning scrapping and pronunciation scrapping

--- src/scapper/CambrideDictionaryScraper.py
+++ src/scapper/CambrideDictionaryScraper.py
@@ -1,5 +1,6 @@
 from DictionaryWebScraperInterface import DictionaryWebScraperInterface
 import requests
+from bs4 import BeautifulSoup
 
 
 class CambridgeDictionaryScraper(DictionaryWebScraperInterface):
@@ -10,7 +11,12 @@ class CambridgeDictionaryScraper(DictionaryWebScraperInterface):
         return super(CambridgeDictionaryScraper, self).scrap(word)
 
     def _extract_pronunciations(self, html_raw_data: str):
-        pass
+        soup = BeautifulSoup(html_raw_data, 'lxml')
+        prounciation_containers = soup.find_all('span', {'class': 'pron dpron'})
+        pronunciations = []
+        for meaning in prounciation_containers:
+            pronunciations.append(self._enliminate_html(meaning.text))
+        return list(dict.fromkeys(pronunciations))
 
     def _get_html_raw_data(self, word: str) -> str:
         return requests.get('https://dictionary.cambridge.org/dictionary/english/' + word, headers={
@@ -18,7 +24,12 @@ class CambridgeDictionaryScraper(DictionaryWebScraperInterface):
         }).text
 
     def _extract_meanings(self, html_raw_data: str):
-        pass
+        soup = BeautifulSoup(html_raw_data, 'lxml')
+        meaning_containers = soup.find_all('div', {'class': 'def ddef_d db'})
+        meanings = []
+        for meaning in meaning_containers:
+            meanings.append(self._enliminate_html(meaning.text))
+        return meanings
 
     def _collect_related_vocabularies(self, html_raw_data: str):
-        pass
+        return ''
--- src/scapper/DictionaryWebScraperInterface.py
+++ src/scapper/DictionaryWebScraperInterface.py
@@ -1,19 +1,23 @@
+import re
+HTML = re.compile('<.*?>|:')
 
-# class _DictionaryWrappedData:
-#     pass
-
+class _DictionaryWrappedData:
+    def __init__(self, meanings, pronunciation, examples, related_words):
+        self.meanings = meanings
+        self.pronunciation = pronunciation
+        self.examples = examples
+        self.related_words = related_words
 
 class DictionaryWebScraperInterface:
     def __init__(self): pass
 
     def scrap(self, word: str):
         html_raw_data = self._get_html_raw_data(word)
-        # meanings = self._extract_meanings(html_raw_data)
-        # pronunciations = self._extract_pronunciations(html_raw_data)
+        meanings = self._extract_meanings(html_raw_data)
+        pronunciations = self._extract_pronunciations(html_raw_data)
         # examples = self._extract_examples(html_raw_data)
-        # related_volcabularies = self._collect_related_vocabularies(html_raw_data)
-        # return _DictionaryWrappedData(meanings, pronunciations, related_volcabularies, examples)
-        return html_raw_data
+        related_words = self._collect_related_vocabularies(html_raw_data)
+        return _DictionaryWrappedData(meanings, pronunciations, related_words, '')
 
     def _extract_pronunciations(self, html_raw_data: str):
         pass
@@ -26,3 +30,6 @@ class DictionaryWebScraperInterface:
 
     def _collect_related_vocabularies(self, html_raw_data: str):
         pass
+
+    def _enliminate_html(self, meaning: str):
+        return re.sub(HTML, '', meaning).strip()
